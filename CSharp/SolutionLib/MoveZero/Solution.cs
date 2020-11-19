6b9659ff5d59e7030270216dcdeec5f71076f428
HathWallace <hathzhou@qq.com>
2020-11-19 20:19:33 +0800

    小修改

--- CSharp/SolutionLib/MoveZero/Solution.cs
+++ CSharp/SolutionLib/MoveZero/Solution.cs
@@ -2,13 +2,12 @@
 {
     public void MoveZeroes(int[] nums)
     {
-        int first0 = -1, last0 = -1;
+        int first0 = -1;
         for (int i = 0; i < nums.Length; i++)
         {
             if (nums[i] == 0)
             {
                 first0 = i;
-                last0 = first0;
                 break;
             }
         }
@@ -17,16 +16,11 @@
 
         for (int i = first0 + 1; i < nums.Length; i++)
         {
-            if (nums[i] == 0)
-            {
-                last0 = i;
-            }
-            else
+            if (nums[i] != 0)
             {
                 nums[first0] = nums[i];
                 nums[i] = 0;
                 first0++;
-                last0++;
             }
         }
 
