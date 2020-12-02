b2f3aba5a3e23f7d22077addb40d01ce4c7c982d
HathWallace <hathzhou@qq.com>
2020-12-02 10:48:53 +0800

    搁置

--- CSharp/SolutionLib/MaxNumber/Solution.cs
+++ CSharp/SolutionLib/MaxNumber/Solution.cs
@@ -6,35 +6,6 @@
         {
             int i1 = 0, i2 = 0, ik = 0;
             var res = new int[k];
-            while (i1 < nums1.Length && i2 < nums2.Length && ik < k)
-            {
-                if (nums1[i1] >= nums2[i2])
-                {
-                    res[ik++] = nums1[i1++];
-                }
-                else
-                {
-                    res[ik++] = nums2[i2++];
-                }
-            }
-
-            if (ik < k)
-            {
-                if (i1 < nums1.Length)
-                {
-                    while (i1 < nums1.Length)
-                    {
-                        res[ik++] = nums1[i1++];
-                    }
-                }
-                else if (i2 < nums2.Length)
-                {
-                    while (i2 < nums2.Length)
-                    {
-                        res[ik++] = nums2[i2++];
-                    }
-                }
-            }
 
             return res;
         }
