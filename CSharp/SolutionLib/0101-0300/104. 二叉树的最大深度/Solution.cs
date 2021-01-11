2976dbc8655c23955540ffc279627d3c995b6989
HathWallace <hathzhou@qq.com>
2021-01-11 09:16:21 +0800

    104. 二叉树的最大深度

---
+++ CSharp/SolutionLib/0101-0300/104. 二叉树的最大深度/Solution.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolutionLib._104._二叉树的最大深度
+{
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            return GetHeight(root);
+        }
+
+        private int GetHeight(TreeNode root)
+        {
+            if (root == null) return 0;
+            return Math.Max(GetHeight(root.left), GetHeight(root.right)) + 1;
+        }
+
+    }
+
+    public class Solution2
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            int ans = 0;
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            while (queue.Count != 0)
+            {
+                ans++;
+                int num = queue.Count;
+                for (int i = 0; i < num; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (node.left != null) queue.Enqueue(node.left);
+                    if (node.right != null) queue.Enqueue(node.right);
+                }
+            }
+            return ans;
+        }
+    }
+}
---
+++ CSharp/SolutionLib/0101-0300/104. 二叉树的最大深度/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._104._二叉树的最大深度
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
