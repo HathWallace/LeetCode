614bd4eff03b9e4a5a4d5158418b1c36b06aad2f
HathWallace <hathzhou@qq.com>
2021-01-12 09:12:14 +0800

    20. 有效的括号

---
+++ CSharp/SolutionLib/0001-0100/20. 有效的括号/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SolutionLib._20._有效的括号
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            int slen = s.Length;
+            if (slen == 0) return true;
+            var stack = new int[slen];
+            var top = -1;
+            for (int i = 0; i < slen; i++)
+            {
+                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                    stack[++top] = i;
+                else
+                {
+                    if (top == -1 || !Match(s[stack[top]], s[i])) return false;
+                    top--;
+                }
+            }
+            return top == -1;
+        }
+
+        private bool Match(char a, char b)
+        {
+            return a == '(' && b == ')' ||
+                   a == '{' && b == '}' ||
+                   a == '[' && b == ']';
+        }
+    }
+}
---
+++ CSharp/SolutionLib/0001-0100/20. 有效的括号/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._20._有效的括号
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
