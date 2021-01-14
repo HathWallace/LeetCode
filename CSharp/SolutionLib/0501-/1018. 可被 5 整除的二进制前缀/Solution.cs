651299a78022d51c65d4503888de96c2c2decb82
HathWallace <hathzhou@qq.com>
2021-01-14 21:13:54 +0800

    1018. 可被 5 整除的二进制前缀

---
+++ CSharp/SolutionLib/0501-/1018. 可被 5 整除的二进制前缀/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace SolutionLib._1018._可被_5_整除的二进制前缀
+{
+    public class Solution2
+    {
+        public IList<bool> PrefixesDivBy5(int[] A)
+        {
+            var res = new List<bool>();
+            int num = 0;
+            foreach (var i in A)
+            {
+                num = ((num << 1) + i) % 5;
+                res.Add(num == 0);
+            }
+            return res;
+        }
+    }
+
+    public class Solution
+    {
+        public IList<bool> PrefixesDivBy5(int[] A)
+        {
+            var res = new List<bool>();
+            int num = 0;
+            foreach (var i in A)
+            {
+                num = (num * 2 + i) % 10;
+                res.Add(num == 0 || num == 5);
+            }
+            return res;
+        }
+    }
+}
---
+++ CSharp/SolutionLib/0501-/1018. 可被 5 整除的二进制前缀/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._1018._可被_5_整除的二进制前缀
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
