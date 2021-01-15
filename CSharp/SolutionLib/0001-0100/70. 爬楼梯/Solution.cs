4828eb08dd0c205f6cd265dabc90b9c182d12707
HathWallace <hathzhou@qq.com>
2021-01-15 22:04:26 +0800

    70. 爬楼梯

---
+++ CSharp/SolutionLib/0001-0100/70. 爬楼梯/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace SolutionLib._70._爬楼梯
+{
+    public class Solution
+    {
+        public int ClimbStairs(int n)
+        {
+            if (n < 2) return 1;
+            int ans = 2, pre = 1;
+            for (int i = 3; i <= n; i++)
+                pre = (ans += pre) - pre;
+            return ans;
+        }
+    }
+}
---
+++ CSharp/SolutionLib/0001-0100/70. 爬楼梯/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._70._爬楼梯
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
