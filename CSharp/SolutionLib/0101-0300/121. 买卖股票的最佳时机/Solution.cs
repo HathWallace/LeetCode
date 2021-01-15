3e417b9a13c71d6e6e93fcb8b360ecf5e0e71f76
HathWallace <hathzhou@qq.com>
2021-01-15 22:10:41 +0800

    121. 买卖股票的最佳时机

---
+++ CSharp/SolutionLib/0101-0300/121. 买卖股票的最佳时机/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SolutionLib._121._买卖股票的最佳时机
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length < 2) return 0;
+            int profit = 0, buy = prices[0];
+            for (int i = 1; i < prices.Length; i++)
+            {
+                buy = Math.Min(buy, prices[i]);
+                profit = Math.Max(profit, prices[i] - buy);
+            }
+            return profit;
+        }
+    }
+}
---
+++ CSharp/SolutionLib/0101-0300/121. 买卖股票的最佳时机/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._121._买卖股票的最佳时机
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
