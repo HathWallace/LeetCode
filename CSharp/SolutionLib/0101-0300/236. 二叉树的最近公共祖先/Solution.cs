fc7901d00cee17fa6980036c7e4d02782f560f6d
HathWallace <hathzhou@qq.com>
2021-01-17 14:05:48 +0800

    236. 二叉树的最近公共祖先

---
+++ CSharp/SolutionLib/0101-0300/236. 二叉树的最近公共祖先/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace SolutionLib._236._二叉树的最近公共祖先
+{
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int x) { val = x; }
+     * }
+     */
+    public class Solution
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null)
+                return null;
+            if (root == p || root == q)
+                return root;
+            var leftRes = LowestCommonAncestor(root.left, p, q);
+            var rightRes = LowestCommonAncestor(root.right, p, q);
+            if (leftRes != null && rightRes != null)
+                return root;
+            return leftRes ?? rightRes;
+        }
+    }
+}
---
+++ CSharp/SolutionLib/0101-0300/236. 二叉树的最近公共祖先/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._236._二叉树的最近公共祖先
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
