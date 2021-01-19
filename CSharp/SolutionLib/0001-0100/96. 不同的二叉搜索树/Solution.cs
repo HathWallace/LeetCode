68b37e55ce683402edc11700587c8431d4aed9da
HathWallace <hathzhou@qq.com>
2021-01-19 20:26:54 +0800

    96. 不同的二叉搜索树

---
+++ CSharp/SolutionLib/0001-0100/96. 不同的二叉搜索树/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._96._不同的二叉搜索树
+{
+    public class Solution
+    {
+        public int NumTrees(int n)
+        {
+            var dp = new int[n + 1];
+            dp[0] = 1;
+            for (int i = 1; i <= n; i++)
+                for (int j = 1; j <= i; j++)
+                    dp[i] += dp[j - 1] * dp[i - j];
+            return dp[n];
+        }
+    }
+}
---
+++ CSharp/SolutionLib/0001-0100/96. 不同的二叉搜索树/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._96._不同的二叉搜索树
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
