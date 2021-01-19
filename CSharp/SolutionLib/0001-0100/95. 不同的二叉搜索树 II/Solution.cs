b5a0bd3e9f482e3937a8348d92cda47fe17b3412
HathWallace <hathzhou@qq.com>
2021-01-19 20:57:16 +0800

    95. 不同的二叉搜索树 II

---
+++ CSharp/SolutionLib/0001-0100/95. 不同的二叉搜索树 II/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace SolutionLib
+{
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public class Solution
+    {
+        public IList<TreeNode> GenerateTrees(int n)
+        {
+            var dp = new List<List<TreeNode>>();
+            dp.Add(new List<TreeNode> { null });
+            for (int i = 1; i <= n; i++)
+            {
+                var tmp = new List<TreeNode>();
+                for (int j = 1; j <= i; j++)
+                    foreach (var _rightNode in dp[i - j])
+                    {
+                        var rightNode = AddVal(_rightNode, j);
+                        foreach (var leftNode in dp[j-1])
+                            tmp.Add(new TreeNode(j, leftNode, rightNode));
+                    }
+                dp.Add(tmp);
+            }
+            return dp[n];
+        }
+
+        private TreeNode AddVal(TreeNode root, int add)
+        {
+            if (root == null) return null;
+            var node = new TreeNode(root.val + add);
+            node.left = AddVal(root.left, add);
+            node.right = AddVal(root.right, add);
+            return node;
+        }
+    }
+}
--- CSharp/SolutionLib/TreeNode.cs
+++ CSharp/SolutionLib/TreeNode.cs
@@ -5,6 +5,12 @@
         public int val;
         public TreeNode left;
         public TreeNode right;
-        public TreeNode(int x) { val = x; }
+
+        public TreeNode(int x, TreeNode left = null, TreeNode right = null)
+        {
+            val = x;
+            this.left = left;
+            this.right = right;
+        }
     }
 }
