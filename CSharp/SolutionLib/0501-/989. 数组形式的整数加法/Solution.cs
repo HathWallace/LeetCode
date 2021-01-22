50e1aa37c894718da6c59ffd7a2be287940ea44c
HathWallace <hathzhou@qq.com>
2021-01-22 10:35:02 +0800

    989. 数组形式的整数加法

---
+++ CSharp/SolutionLib/0501-/989. 数组形式的整数加法/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SolutionLib._989._数组形式的整数加法
+{
+    public class Solution
+    {
+        public IList<int> AddToArrayForm(int[] A, int K)
+        {
+            var res = new List<int>();
+            int add = 0;
+            for (int i = A.Length - 1; i >= 0; i--)
+            {
+                int v = A[i] + add + K % 10;
+                A[i] = v % 10;
+                add = v / 10;
+                K /= 10;
+                if (add == 0 && K == 0) return A;
+            }
+            while (add != 0 || K != 0)
+            {
+                int v = add + K % 10;
+                res.Add(v % 10);
+                add = v / 10;
+                K /= 10;
+            }
+            res.Reverse();
+            res.AddRange(A);
+            return res.ToList();
+        }
+    }
+}
---
+++ CSharp/SolutionLib/0501-/989. 数组形式的整数加法/Test.cs
@@ -0,0 +1,14 @@
+﻿namespace SolutionLib._989._数组形式的整数加法
+{
+    public class Test
+    {
+        public static void Run()
+        {
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
