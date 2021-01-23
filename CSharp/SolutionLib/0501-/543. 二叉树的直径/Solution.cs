5cf5adf89787cc9c44843c9204c9a62c8f238ad4
HathWallace <hathzhou@qq.com>
2021-01-23 20:19:21 +0800

    543. 二叉树的直径

---
+++ CSharp/SolutionLib/0501-/543. 二叉树的直径/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SolutionLib._543._二叉树的直径
+{
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int x) { val = x; }
+     * }
+     */
+    public class Solution
+    {
+        public int DiameterOfBinaryTree(TreeNode root)
+        {
+            int ans = 0;
+            GetHeight(root, ref ans);
+            return ans;
+        }
+
+        private int GetHeight(TreeNode root, ref int ans)
+        {
+            if (root == null) return 0;
+            int leftH = GetHeight(root.left, ref ans);
+            int rightH = GetHeight(root.right, ref ans);
+            ans = Math.Max(ans, leftH + rightH);
+            return Math.Max(leftH, rightH) + 1;
+        }
+
+    }
+}
---
+++ CSharp/SolutionLib/0501-/543. 二叉树的直径/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._543._二叉树的直径
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
