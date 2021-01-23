cff7a6539093a09d4722d8be9d78887f3c393d20
HathWallace <hathzhou@qq.com>
2021-01-23 21:32:11 +0800

    538. 把二叉搜索树转换为累加树

---
+++ CSharp/SolutionLib/0501-/538. 把二叉搜索树转换为累加树/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace SolutionLib._538._把二叉搜索树转换为累加树
+{
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public class Solution
+    {
+        public TreeNode ConvertBST(TreeNode root)
+        {
+            int add = 0;
+            GreaterSumTree(root, ref add);
+            return root;
+        }
+
+        private void GreaterSumTree(TreeNode root, ref int add)
+        {
+            if (root == null) return;
+            GreaterSumTree(root.right, ref add);
+            add = root.val += add;
+            GreaterSumTree(root.left, ref add);
+        }
+    }
+
+}
---
+++ CSharp/SolutionLib/0501-/538. 把二叉搜索树转换为累加树/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._538._把二叉搜索树转换为累加树
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
