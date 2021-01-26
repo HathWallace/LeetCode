00cd396323e833891ec33769df3c42b3b69d687b
HathWallace <hathzhou@qq.com>
2021-01-26 13:09:31 +0800

    1128. 等价多米诺骨牌对的数量

---
+++ CSharp/SolutionLib/0501-/1128. 等价多米诺骨牌对的数量/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolutionLib._1128._等价多米诺骨牌对的数量
+{
+    public class Solution
+    {
+        public int NumEquivDominoPairs(int[][] dominoes)
+        {
+            int ans = 0, n = dominoes.Length;
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int x = dominoes[i][0], y = dominoes[i][1];
+                int index = Math.Min(x * n + y, x + y * n);
+                if (!dict.ContainsKey(index))
+                    dict[index] = 0;
+                ans += dict[index];
+                dict[index]++;
+            }
+            return ans;
+        }
+    }
+}
---
+++ CSharp/SolutionLib/0501-/1128. 等价多米诺骨牌对的数量/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._1128._等价多米诺骨牌对的数量
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
