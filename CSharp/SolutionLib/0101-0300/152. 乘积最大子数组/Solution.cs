a7621035102ff403168d2c6618ae91c459756b41
HathWallace <hathzhou@qq.com>
2021-01-27 14:13:06 +0800

    152. 乘积最大子数组

---
+++ CSharp/SolutionLib/0101-0300/152. 乘积最大子数组/Solution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SolutionLib._152._乘积最大子数组
+{
+    public class Solution2
+    {
+        public int MaxProduct(int[] nums)
+        {
+            int n = nums.Length, ans = int.MinValue, maxF = 1, minF = 1;
+            foreach (var num in nums)
+            {
+                int tmp = maxF;
+                maxF = Math.Max(num, Math.Max(num * maxF, num * minF));
+                minF = Math.Min(num, Math.Min(num * tmp, num * minF));
+                ans = Math.Max(ans, maxF);
+            }
+            return ans;
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxProduct(int[] nums)
+        {
+            int n = nums.Length, ans;
+            var max = new int[n];
+            var min = new int[n];
+            ans = max[0] = min[0] = nums[0];
+            for (int i = 1; i < n; i++)
+            {
+                var tmp = new[] { max[i - 1] * nums[i], min[i - 1] * nums[i], nums[i] };
+                Array.Sort(tmp);
+                max[i] = tmp[2];
+                min[i] = tmp[0];
+                ans = Math.Max(ans, max[i]);
+            }
+            return ans;
+        }
+    }
+
+    //超出时间限制
+    public class _Solution
+    {
+        public int MaxProduct(int[] nums)
+        {
+            int ans = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int dp = 1;
+                for (int j = i; j >= 0; j--)
+                    ans = Math.Max(ans, dp *= nums[j]);
+            }
+            return ans;
+        }
+    }
+
+}
---
+++ CSharp/SolutionLib/0101-0300/152. 乘积最大子数组/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._152._乘积最大子数组
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
