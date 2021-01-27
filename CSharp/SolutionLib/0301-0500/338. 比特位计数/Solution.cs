019d3da392aebb206fa8283d751140acb9f47787
HathWallace <hathzhou@qq.com>
2021-01-27 09:43:15 +0800

    338. 比特位计数

---
+++ CSharp/SolutionLib/0301-0500/338. 比特位计数/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace SolutionLib._338._比特位计数
+{
+    public class Solution2
+    {
+        public int[] CountBits(int num)
+        {
+            var res = new int[num + 1];
+            if (num == 0) return res;
+            var queue = new Queue<int>();
+            res[1] = 1;
+            queue.Enqueue(1);
+            while (queue.Count != 0)
+            {
+                int pre = queue.Dequeue(), tmp = pre * 2;
+                if (tmp > num) continue;
+                res[tmp] = res[pre];
+                queue.Enqueue(tmp++);
+                if (tmp > num) continue;
+                res[tmp] = res[pre] + 1;
+                queue.Enqueue(tmp);
+            }
+            return res;
+        }
+    }
+
+    public class Solution
+    {
+        public int[] CountBits(int num)
+        {
+            var res = new int[num + 1];
+            Divide(res, 1, 1);
+            return res;
+        }
+
+        private void Divide(int[] res, int num, int oneCount)
+        {
+            if (num >= res.Length) return;
+            res[num] = oneCount;
+            Divide(res, 2 * num, oneCount);
+            Divide(res, 2 * num + 1, oneCount + 1);
+        }
+
+    }
+}
---
+++ CSharp/SolutionLib/0301-0500/338. 比特位计数/Test.cs
@@ -0,0 +1,15 @@
+﻿namespace SolutionLib._338._比特位计数
+{
+    public class Test
+    {
+        public static void Run()
+        {
+
+        }
+
+        /*
+         * 测试用例：
+         */
+
+    }
+}
