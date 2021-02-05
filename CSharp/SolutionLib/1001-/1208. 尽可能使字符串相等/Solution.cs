4409596bbcbfa1d513007f22fb8c16574c7ec1b0
HathWallace <53185608+HathWallace@users.noreply.github.com>
2021-02-05 19:49:27 +0800

    1208. 尽可能使字符串相等

---
+++ CSharp/SolutionLib/1001-/1208. 尽可能使字符串相等/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SolutionLib._1208._尽可能使字符串相等
+{
+    public class Solution
+    {
+        public int EqualSubstring(string s, string t, int maxCost)
+        {
+            int left = 0, right = 0, slen = s.Length, cost = 0;
+            while (right < slen)
+            {
+                cost += Math.Abs(s[right] - t[right]);
+                if (cost > maxCost)
+                {
+                    cost -= Math.Abs(s[left] - t[left]);
+                    left++;
+                }
+                right++;
+            }
+            return right - left;
+        }
+    }
+}
