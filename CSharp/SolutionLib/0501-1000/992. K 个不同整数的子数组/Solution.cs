b573ada98bce5a674f85444be6db6542a56d7210
HathWallace <53185608+HathWallace@users.noreply.github.com>
2021-02-11 16:55:50 +0800

    992. K 个不同整数的子数组

---
+++ CSharp/SolutionLib/0501-1000/992. K 个不同整数的子数组/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace SolutionLib._992._K_个不同整数的子数组
+{
+    public class Solution
+    {
+        public int SubarraysWithKDistinct(int[] A, int K)
+        {
+            return -1;
+        }
+    }
+
+    //超出时间限制
+    public class _Solution
+    {
+        public int SubarraysWithKDistinct(int[] A, int K)
+        {
+            int len = A.Length, ans = 0;
+            for (int i = 0; i < len; i++)
+            {
+                var window = new HashSet<int>();
+                for (int j = i; j >= 0 && window.Count <= K; j--)
+                {
+                    int num = A[j];
+                    window.Add(num);
+                    if (window.Count == K) ans++;
+                }
+            }
+            return ans;
+        }
+    }
+
+}
---
+++ CSharp/SolutionLib/0501-1000/992. K 个不同整数的子数组/Solution_doublept.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace SolutionLib._992._K_个不同整数的子数组
+{
+    public class Solution_doublept
+    {
+        public int SubarraysWithKDistinct(int[] A, int K)
+        {
+            return AtMostKDistinct(A, K) - AtMostKDistinct(A, K - 1);
+        }
+
+        public int AtMostKDistinct(int[] A, int K)
+        {
+            int left = 0, right = 0, len = A.Length, valid = 0, ans = 0;
+            var windows = new Dictionary<int, int>();
+            while (right < len)
+            {
+                int rnum = A[right++];
+                if (!windows.ContainsKey(rnum)) windows[rnum] = 0;
+                windows[rnum]++;
+                if (windows[rnum] == 1) valid++;
+                while (valid > K)
+                {
+                    int lnum = A[left++];
+                    if (windows[lnum] == 1) valid--;
+                    windows[lnum]--;
+                }
+                ans += right - left;
+            }
+            return ans;
+        }
+
+    }
+}
