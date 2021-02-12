23e44aa08056f80a6cf7fcf69b2f2c0de7d8986e
HathWallace <53185608+HathWallace@users.noreply.github.com>
2021-02-12 10:54:11 +0800

    118. 杨辉三角

---
+++ CSharp/SolutionLib/0101-0200/118. 杨辉三角/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace SolutionLib._118._杨辉三角
+{
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            var res = new List<IList<int>>();
+            if (numRows == 0) return res;
+            res.Add(new List<int> { 1 });
+            for (int i = 1; i < numRows; i++)
+            {
+                var list = new List<int>();
+                list.Add(1);
+                for (int j = 1; j < i; j++)
+                    list.Add(res[i - 1][j - 1] + res[i - 1][j]);
+                list.Add(1);
+                res.Add(list);
+            }
+            return res;
+        }
+    }
+}
