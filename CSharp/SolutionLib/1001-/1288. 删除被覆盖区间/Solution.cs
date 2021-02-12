7fbddef19a754a945b7d476d4a98fe8a3e12bc8b
HathWallace <53185608+HathWallace@users.noreply.github.com>
2021-02-12 16:42:09 +0800

    1288. 删除被覆盖区间

---
+++ CSharp/SolutionLib/1001-/1288. 删除被覆盖区间/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SolutionLib._1288._删除被覆盖区间
+{
+    public class Solution
+    {
+        public int RemoveCoveredIntervals(int[][] intervals)
+        {
+            int ans = 1, n = intervals.Length;
+            if (n == 0) return 0;
+            Array.Sort(intervals, (a, b) =>
+            {
+                if (a[0] != b[0]) return a[0].CompareTo(b[0]);
+                return b[1].CompareTo(a[1]);
+            });
+            var tmp = intervals[0];
+            for (int i = 1; i < n; i++)
+            {
+                if(Cover(tmp,intervals[i])) continue;
+                ans++;
+                tmp = intervals[i];
+            }
+            return ans;
+        }
+
+        private bool Cover(int[] tmp, int[] interval)
+        {
+            return tmp[0] <= interval[0] && tmp[1] >= interval[1];
+        }
+    }
+}
