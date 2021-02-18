b7ab9eb6758bd53334200bc658943c8f83c3bcc9
HathWallace <53185608+HathWallace@users.noreply.github.com>
2021-02-18 17:56:18 +0800

    995. K 连续位的最小翻转次数

---
+++ CSharp/SolutionLib/0801-1000/995. K 连续位的最小翻转次数/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace SolutionLib._995._K_连续位的最小翻转次数
+{
+    public class Solution
+    {
+        public int MinKBitFlips(int[] A, int K)
+        {
+            return -1;
+        }
+    }
+
+    //超出时间限制
+    public class _Solution
+    {
+        public int MinKBitFlips(int[] A, int K)
+        {
+            int ans = 0;
+            for (int i = 0; i + K <= A.Length; i++)
+            {
+                if (A[i] == 1) continue;
+                ans++;
+                for (int j = 0; j < K; j++)
+                    A[i + j] = (A[i + j] + 1) % 2;
+            }
+            for (int i = 0; i < A.Length; i++)
+                if (A[i] == 0) return -1;
+            return ans;
+        }
+    }
+
+}
---
+++ CSharp/SolutionLib/0801-1000/995. K 连续位的最小翻转次数/Solution_slidewindow.cs
@@ -0,0 +1,48 @@
+﻿namespace SolutionLib._995._K_连续位的最小翻转次数
+{
+    public class Solution_slidewindow
+    {
+        public int MinKBitFlips(int[] A, int K)
+        {
+            int n = A.Length, ans = 0, revCnt = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (i - K >= 0 && A[i - K] > 1)
+                {
+                    revCnt ^= 1;
+                    //A[i - K] -= 2;
+                }
+                if (A[i] == revCnt)
+                {
+                    if (i + K > n) return -1;
+                    ans++;
+                    revCnt ^= 1;
+                    A[i] += 2;
+                }
+            }
+            return ans;
+        }
+    }
+
+    public class _Solution_slidewindow
+    {
+        public int MinKBitFlips(int[] A, int K)
+        {
+            int n = A.Length, ans = 0, revCnt = 0;
+            var diff = new int[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                revCnt ^= diff[i];
+                if (A[i] == revCnt)
+                {
+                    if (i + K > n) return -1;
+                    ans++;
+                    revCnt ^= 1;
+                    diff[i + K] ^= 1;
+                }
+            }
+            return ans;
+        }
+    }
+
+}
